9a52d18dd7d8b15fe44d65616ff8096c2c509b09
Caio <caiomateux@hotmail.com>
2021-11-15 19:39:50 -0300

    Login system implemented

--- website/views.py
+++ website/views.py
@@ -1,12 +1,24 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, logout_user, current_user
+from .models import Note
+from . import db
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
+@login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
 
-    return render_template("home.html")
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note= Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+    return render_template("home.html", user=current_user)
 
 
 
