f7bf4146c7188b5e9451838f60d623d059662159
Caio <caiomateux@hotmail.com>
2021-11-27 20:58:06 -0300

    make it better

---
+++ website/main.py
@@ -0,0 +1,10 @@
+#pip3 install flask
+#pip3 install flask-sqlalchemy
+#pip3 install flask-logingit remote add origin https://github.com/caiomateux/agendamento_web.git
+
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000,debug=True)
