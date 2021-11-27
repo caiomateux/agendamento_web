604086fb7321392cbf7457d08a6ae13ef58504e7
Caio <caiomateux@hotmail.com>
2021-11-27 18:57:03 -0300

    database changes

--- website/cal.py
+++ website/cal.py
@@ -30,7 +30,7 @@ def add():
     return redirect(url_for('cal.calendar'))
 
 
-@cal.route('/update/', methods=['GET', 'POST'])
+@cal.route('/update', methods=['GET', 'POST'])
 def update():
     if request.method == 'POST':
         my_events = Event.query.get(request.form.get('id'))
@@ -47,7 +47,7 @@ def update():
         return redirect(url_for('cal.calendar'))
 
 
-@cal.route('/delete', methods=['GET', 'POST'])
+@cal.route('//delete/<id>/', methods=['GET', 'POST'])
 def delete(id):
     my_events = Event.query.get(id)
     db.session.delete(my_events)
